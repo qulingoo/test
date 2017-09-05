4ccf2d1118956aa6237a5afe853e27cf24b725f4
qulingkang <lingkang.qu@shuyun.com>
2017-09-05 18:10:32 +0800

    update code

--- src/test/java/com/qlk/test/Test.java
+++ src/test/java/com/qlk/test/Test.java
@@ -1,23 +1,13 @@
 package com.qlk.test;
 
-import com.qlk.dao.model.ITableModelDao;
-import com.qlk.po.TableModel;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.UUID;
 
 public class Test {
     public static void main(String args[]) {
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[]{"spring.xml", "spring-mvc.xml"});
-        ITableModelDao iTableModelDao = (ITableModelDao) context.getBean("iTableModelDao");
-        TableModel model = new TableModel();
-        model.setId(UUID.randomUUID().toString());
-        model.setDescribes("123");
-        model.setTableNameChn("abc");
-        model.setTableNameEng("abc");
-        iTableModelDao.insert(model);
+        
     }
 
 }

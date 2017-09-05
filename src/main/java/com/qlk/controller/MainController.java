e960f584233fcc424ec20465b92d39382a47fb26
qulingkang <lingkang.qu@shuyun.com>
2017-09-05 09:33:03 +0800

    update code

--- src/main/java/com/qlk/controller/MainController.java
+++ src/main/java/com/qlk/controller/MainController.java
@@ -9,6 +9,7 @@ public class MainController {
     @ResponseBody
     @RequestMapping("/main/hello")
     public String main() {
-        return "hello world123";
+        return "hello world123123";
     }
+
 }

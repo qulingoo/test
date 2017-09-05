8ba1f35582ba3b4f6cea0e69ed723f6f4cc957a8
qulingkang <lingkang.qu@shuyun.com>
2017-09-05 16:19:22 +0800

    update code

--- src/main/java/com/qlk/dao/model/impl/ITableModelTemplateDaoImpl.java
+++ src/main/java/com/qlk/dao/model/impl/ITableModelTemplateDaoImpl.java
@@ -1,4 +1,4 @@
-package com.qlk.dao.impl;
+package com.qlk.dao.model.impl;
 
 import com.qlk.dao.model.ITableModelDao;
 import com.qlk.po.TableModel;
@@ -14,20 +14,14 @@ public class ITableModelTemplateDaoImpl implements ITableModelDao {
 
     @Override
     public int insert(TableModel tableModel) {
-        String query = "insert into TABLE_MODEL (ID,TABLE_NAME_CHN,TABLE_NAME_ENG, DESCRIBES) values (?,?,?,?)";
+        String insert = "insert into TABLE_MODEL (ID,TABLE_NAME_CHN,TABLE_NAME_ENG, DESCRIBES) values (?,?,?,?)";
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
         Object[] args = new Object[]{tableModel.getId(), tableModel.getTableNameChn(), tableModel.getTableNameEng(), tableModel.getDescribes()};
 
-        int out = jdbcTemplate.update(query, args);
-
-        if (out != 0) {
-            System.out.println("Employee saved with id=" + tableModel.getId());
-        } else {
-            System.out.println("Employee save failed with id=" + tableModel.getId());
-        }
-        return 0;
+        int out = jdbcTemplate.update(insert, args);
+        return out;
     }
 
     @Override
